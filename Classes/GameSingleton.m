c637e4bcc4755a46ed3807bdec3f4b23baadbf70
jonp101 <42489212+jonp101@users.noreply.github.com>
2018-08-18 09:54:26 +0100

    Update GameSingleton.m

--- Classes/GameSingleton.m
+++ Classes/GameSingleton.m
@@ -1,9 +1,7 @@
 //
 //  GameSingleton.m
-//  flickit
-//
+//  Klink
 //  Created by My Mac on 20/06/2010.
-//  Copyright 2010 __MyCompanyName__. All rights reserved.
 //
 
 #import "GameSingleton.h"
